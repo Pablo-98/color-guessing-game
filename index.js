74c035a2578cb4018ffc9e9d3eecfee18f43ae01
Pablo Hernandez <hpablo575@gmail.com>
2022-09-18 23:53:38 -0600

    added variables to gamestart button

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('yo');
+const colors_array = ['blue', 'green', 'black','gray',
+'purple','red','orange'];
+
+// declaring the function id that will be used when the button is clicked'
+function gameStart(){
+    let guess = "";
+    let correct = false;
+    // declaring target index that will be called in the target function. Returning a random color
+    const targetIndex = Math.floor(Math.random() * colors_array.length());
+    // declaring the target variable which will be a random color I think
+    const target = colors_array[targetIndex];
+
+    console.log(target)
+}
